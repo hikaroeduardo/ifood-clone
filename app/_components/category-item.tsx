5997f449d90b64affa627c94e1cc24708ed4d7f5
Hikaro Eduardo <hikaroeduardobandeira@gmail.com>
2024-05-08 12:13:33 -0300

    style: add scrolling in category list

--- app/_components/category-item.tsx
+++ app/_components/category-item.tsx
@@ -7,7 +7,7 @@ interface CategoryItemProps {
 
 const CategoryItem = ({ category }: CategoryItemProps) => {
   return (
-    <div className="flex items-center gap-3 py-3 px-4 bg-white shadow-md rounded-full">
+    <div className="flex items-center gap-3 py-3 px-4 bg-white border-2 rounded-full min-w-[200px] w-full">
       <Image
         src={category.imageUrl}
         alt={category.name}
