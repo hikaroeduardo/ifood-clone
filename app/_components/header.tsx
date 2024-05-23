b08ca86aee5584a9e3e77a9d1f7d39f26c80c3a5
Hikaro Eduardo <hikaro.bandeira@etipi.pi.gov.br>
2024-05-23 11:36:06 -0300

    feat: add redirect to home in logo

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,12 +1,15 @@
+import Link from "next/link";
 import { Button } from "./ui/button";
 import { MenuIcon } from "lucide-react";
 
 const Header = () => {
   return (
     <div className="flex justify-between pt-6 px-5">
-      <h1 className="font-bold text-2xl">
-        Seu<span className="text-primary">Lanche</span>.com
-      </h1>
+      <Link href="/">
+        <h1 className="font-bold text-2xl">
+          Seu<span className="text-primary">Lanche</span>.com
+        </h1>
+      </Link>
 
       <Button
         size="icon"
